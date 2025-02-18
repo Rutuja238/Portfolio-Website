f4d0ce087e2ee19c2d3686054a1965ef168796a5
Rutuja238 <narrutuja@gmail.com>
2025-02-18 21:23:53 +0530

    fix: update download resume

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         <h1>Rutuja</h1>
         <p>Full-Stack Developer | Passionate About Building Scalable Applications</p>
         <div className="buttons">
-          <a href="https://drive.google.com/file/d/1eHVZRshkYqH1Bx_6HuSvEiQIf79KxrKZ/view?usp=drive_link" download className="btn">Download Resume</a>
+          <a href="https://drive.google.com/uc?export=download&id=1eHVZRshkYqH1Bx_6HuSvEiQIf79KxrKZ" className="btn">Download Resume</a>
           <a href="#contact" className="btn btn-alt">Contact Me</a>
         </div>
       </div>
